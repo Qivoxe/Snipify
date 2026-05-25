2648db9917b64c9f2b28cfbd4ecdfd55d051fd56
unknown <shivamthechad@gmail.com>
2026-05-25 13:40:31 +0530

    table creation

--- app/main.py
+++ app/main.py
@@ -1,19 +1,38 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routers import urls
+from app.database import engine, Base
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    # Shutdown (optional)
+    await engine.dispose()
+
 
 app = FastAPI(
-    title = "Snipify",
-    description = "Real-time URL shortner with analytics",
-    version = "1.0.0"
+    title="Snipify",
+    description="Real-time URL shortner with analytics",
+    version="1.0.0",
+    lifespan=lifespan
 )
 
 app.include_router(urls.router)
 
+
 @app.get("/")
 def home():
-    return {"message": "Welcome to Snipify "}
+    return {"message": "Welcome to Snipify"}
+
 
 @app.get("/health")
 def health():
-    return {"status":"ok", "app":"Snipify"}
+    return {"status": "ok", "app": "Snipify"}
+
 
